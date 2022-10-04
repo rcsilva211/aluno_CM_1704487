1b44bc802433d2c37aba6147e8748efd416e980e
rcsilva211 <rodrigofx211@gmail.com>
2022-10-04 17:51:58 +0100

    Page 2 button fix + intent

--- android/App2/app/src/main/java/com/example/app2/MainActivity2.java
+++ android/App2/app/src/main/java/com/example/app2/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void Retroceder(View view) {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void Alterar(View view) {
+        Intent intent=new Intent(this,MainActivity2.class);
+        startActivity(intent);
     }
 }
