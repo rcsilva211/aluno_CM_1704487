40a89a8902104392c74b254f55fe16544d26cf39
rcsilva211 <rodrigofx211@gmail.com>
2022-10-11 17:23:27 +0100

    button send add

--- android/App3_2/app/src/main/java/com/example/app3_2/MainActivity2.java
+++ android/App3_2/app/src/main/java/com/example/app3_2/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.app3_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+
+    public void send4(View view) {
+    }
+
+    public void send5(View view) {
+    }
+
+    public void send6(View view) {
+    }
+
+    public void send7(View view) {
     }
 }
